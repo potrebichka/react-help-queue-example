a3bcec902ffbf96a2fc24e9b7926afabd038230f
Nina Potrebich <potrebich@gmail.com>
2020-02-19 11:22:09 -0800

    Add Updating time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import TicketList from './TicketList';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 import NewTicketControl from './NewTicketControl';
+import Moment from 'moment';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,8 +15,27 @@ class App extends React.Component {
     this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
   }
 
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() => 
+      this.updateTicketElapsedWaitTime(), 60000);
+  }
+
+  updateTicketElapsedWaitTime() {
+    console.log("check");
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) => 
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );  
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
   handleAddingNewTicketToList(newTicket) {
     var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
     newMasterTicketList.push(newTicket);
     this.setState({masterTicketList: newMasterTicketList});
   }
